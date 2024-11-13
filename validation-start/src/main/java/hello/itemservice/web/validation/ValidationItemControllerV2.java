378cbb07f44f28ca32513fd8eb7c553353216d8b
Gyubum Kim <kgb2872@gmail.com>
2024-11-12 22:57:51 -0500

    validation_practice_in_Spring
    Used to code and arguments options on FieldError() to make my life easier

--- validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -31,22 +31,23 @@ public class ValidationItemControllerV2 {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "validation/v1/items";
+        return "validation/v2/items";
     }
 
     @GetMapping("{itemID}")
     public String item(@PathVariable Long itemID, Model model) {
         Item item = itemRepository.findById(itemID);
         model.addAttribute("item", item);
-        return "validation/v1/item";
+        return "validation/v2/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
-        return "validation/v1/addItem";
+        return "validation/v2/addForm";
     }
 
+    /*
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, BindingResult  bindingResult, RedirectAttributes redirectAttributes) {
 
@@ -68,6 +69,38 @@ public class ValidationItemControllerV2 {
 
             if (resultPrice < 10000) {
               bindingResult.addError(new ObjectError("item","Price must be between 1 and 10000"));
+            }
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addFlashAttribute("savedItem", savedItem.getId());
+        redirectAttributes.addFlashAttribute("status", true);
+
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+    */
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult  bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item","price",item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1, 1000000}, null));
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() >= 10000) {
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(), false, new String[]{"max.item.quantity"},new Object[]{1, 10000}, null));
+        }
+
+        //Global vaildation
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice},null));
             }
         }
 
